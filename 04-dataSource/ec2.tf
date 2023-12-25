c9301cf221f97452fa9d06873fce9d77120ce49b
Shoaib <shoaibs411@gmail.com>
2023-12-26 04:16:36 +0530

    Storing Statefile centrally on S3 bucket on AWS

--- 04-dataSource/ec2.tf
+++ 04-dataSource/ec2.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "app" {
+resource "aws_instance" "web" {
   ami                       = data.aws_ami.ami.id           # This is arguments
   instance_type             = "t2.micro"
   vpc_security_group_ids  = [aws_security_group.allow_tls.id]
 
   tags = {
-    Name                    = "DevOps with AWS LabInstance"
+    Name                    = "AWS EC-2 LabInstance"
   }
 }
 
