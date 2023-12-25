d05cd349a42c0741d52c0404d55f56c073746044
Shoaib <shoaibs411@gmail.com>
2023-12-26 02:56:32 +0530

    updating ec-2 instance type to t2.micro for free usage :)

--- 03-ec2-instance/ec2.tf
+++ 03-ec2-instance/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-0f75a13ad2e340a58"
-  instance_type = "t2.medium"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "DevOps with AWS LabInstance"
