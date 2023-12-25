547a5e77df9ac6fe1a6f0651b9133ce86b8cf6c3
Shoaib <shoaibs411@gmail.com>
2023-12-26 03:31:43 +0530

    Assigning the created Security group to the instance

--- 03-ec2-instance/ec2.tf
+++ 03-ec2-instance/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
   ami                       = "ami-0f75a13ad2e340a58"           # This is arguments
   instance_type             = "t2.micro"
-  vpc_security_group_ids  = ["aws_security_group.allow_tls.id"]
-  
+  vpc_security_group_ids  = [aws_security_group.allow_tls.id]
+
   tags = {
     Name                    = "DevOps with AWS LabInstance"
   }
