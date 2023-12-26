735b7af3126110c25e5b574de77919ef599c81d7
Shoaib <shoaibs411@gmail.com>
2023-12-27 00:34:19 +0530

    Example on remoteProvisioner running ansible playbook on a newly created ec2 machine

---
+++ 06-remoteModules/ec2/remoteProvsioner.tf
@@ -0,0 +1,19 @@
+resource "null_resource" "playbook" {
+
+    depends_on = [aws_instance.app]
+
+    connection {
+        type     = "ssh"
+        user     = "centos"
+        password = "DevOps321"
+        host     = aws_instance.app.private_ip
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo yum install -y epel-release",
+            "sudo yum install -y ansible",
+            "ansible-pull -U https://github.com/Shoaibs411/ansible.git -e ENV=dev -e COMPONENT=mongodb roboshop-pull.yml"
+        ] 
+    }
+}
