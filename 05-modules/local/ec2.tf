e34998f01ae24ae4cd5122682bfaab0c970ecfb3
Shoaib <shoaibs411@gmail.com>
2023-12-26 19:00:15 +0530

    Trying to print public IP through local module

--- 05-modules/local/ec2.tf
+++ 05-modules/local/ec2.tf
@@ -2,3 +2,7 @@ resource "aws_instance" "app" {
   ami                       = "ami-0f75a13ad2e340a58"           # This is arguments
   instance_type             = "t2.micro"
 }
+
+output "public_ip"{
+    value = aws_instance.app.public_ip
+}
