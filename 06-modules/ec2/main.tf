23bb94e44f30cce6714d7865dd0b3f5a73229a4c
Shoaib <shoaibs411@gmail.com>
2023-12-26 19:31:46 +0530

    fetching values 2 different modules through main module

--- 06-modules/ec2/main.tf
+++ 06-modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "app" {
   ami                       = "ami-0f75a13ad2e340a58"           # This is arguments
   instance_type             = "t2.micro"
-  vpc_security_group_ids    = [var.sg]
+  vpc_security_group_ids    = [var.sg]                          # Start using the variable
 
   tags = {
     Name                    = "EC-2 from Modules"
@@ -10,11 +10,10 @@ resource "aws_instance" "app" {
 
 
 
-# Declare the input that we have to consume as a variable
+# Step 3: Declare the input that we have to consume as a variable
 variable "sg" {}
 
 
-# Info which needs to be shared to other module has to be decalred as an output. 
 output "public_ip"{
     value = aws_instance.app.public_ip
 }
