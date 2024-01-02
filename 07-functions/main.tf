f40c29a732c30ea668c72ad122525133a1750aac
Shoaib <shoaibs411@gmail.com>
2024-01-02 23:58:19 +0530

    Exploring Functions in Terraform

--- 07-functions/main.tf
+++ 07-functions/main.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "web" {
+  count                     = 0
   ami                       = data.aws_ami.ami.id           # This is arguments
   instance_type             = "t2.micro"
 
