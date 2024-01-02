1cd82c9dc656c078f5284d053b9f083a48fabcb8
Shoaib <shoaibs411@gmail.com>
2024-01-02 23:51:22 +0530

    Exploring Functions in Terraform

--- 07-functions/main.tf
+++ 07-functions/main.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "web" {
   ami                       = data.aws_ami.ami.id           # This is arguments
   instance_type             = "t2.micro"
-  vpc_security_group_ids  = [aws_security_group.allow_tls.id]
 
   tags = {
     Name                    = "AWS DevOps EC-2 LabInstance"
