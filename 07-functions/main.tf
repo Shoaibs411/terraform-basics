7be6d46da7f70f9cb0e1b560e76fe313b4020f84
Shoaib <shoaibs411@gmail.com>
2024-01-03 00:09:00 +0530

    Creating 3 instance for Catalogue,Cart & User

--- 07-functions/main.tf
+++ 07-functions/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  count                     = 3
+  count                     = 0                             # provide the number to create those many instances
   ami                       = data.aws_ami.ami.id           # This is arguments
   instance_type             = "t2.micro"
 
