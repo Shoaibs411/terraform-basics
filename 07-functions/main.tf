381b20c5b0340788f975f56eedc4c3a19b59123c
Shoaib <shoaibs411@gmail.com>
2024-01-03 00:02:21 +0530

    Exploring Functions in Terraform

--- 07-functions/main.tf
+++ 07-functions/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "web" {
-  count                     = 0
+  count                     = 3
   ami                       = data.aws_ami.ami.id           # This is arguments
   instance_type             = "t2.micro"
 
   tags = {
-    Name                    = "AWS DevOps EC-2 LabInstance"
+    Name                    = "AWS DevOps EC-2 LabInstance-${count.index+1}"
   }
 }
