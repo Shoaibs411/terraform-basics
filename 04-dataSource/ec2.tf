ba967e476cb8103383fe4bb075e4a944f349183f
Shoaib <shoaibs411@gmail.com>
2024-01-02 23:50:17 +0530

    Exploring Functions in Terraform

--- 04-dataSource/ec2.tf
+++ 04-dataSource/ec2.tf
@@ -1,15 +1,9 @@
 resource "aws_instance" "web" {
-  ami                       = data.aws_ami.ami.id           # This is arguments
-  instance_type             = "t2.micro"
-  vpc_security_group_ids  = [aws_security_group.allow_tls.id]
+  ami                    =  data.aws_ami.ami.id                                # argument
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allows_tls.id]
 
-  tags = {
-    Name                    = "AWS DevOps EC-2 LabInstance"
-  }
-}
-
-data "aws_ami" "ami" {
-  most_recent      = true
-  name_regex       = "DevOps-LabImage-CentOS7"
-  owners           = ["355449129696"]
+    tags = {
+             Name        = "AWSCloud-LabInstance"
+    }
 }
