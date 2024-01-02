5e8555bfe2eb6940e241fc51c372ee1b2b8b3637
Shoaib <shoaibs411@gmail.com>
2024-01-02 23:46:24 +0530

    Exploring Functions in Terraform

---
+++ 07-functions/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "web" {
+  ami                       = data.aws_ami.ami.id           # This is arguments
+  instance_type             = "t2.micro"
+  vpc_security_group_ids  = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name                    = "AWS DevOps EC-2 LabInstance"
+  }
+}
+
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "DevOps-LabImage-CentOS7"
+  owners           = ["355449129696"]
+}
